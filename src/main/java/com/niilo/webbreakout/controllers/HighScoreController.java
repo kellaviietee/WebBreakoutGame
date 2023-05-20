8cbf8bcabdba6b8e5327dc7945b76e01bb65fec2
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2023-05-20 18:04:21 +0300

    Added getMapping for highscores

--- src/main/java/com/niilo/webbreakout/controllers/HighScoreController.java
+++ src/main/java/com/niilo/webbreakout/controllers/HighScoreController.java
@@ -1,9 +1,16 @@
 package com.niilo.webbreakout.controllers;
 
+import com.niilo.webbreakout.entities.HighScore;
 import com.niilo.webbreakout.services.HighScoreService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HighScoreController {
 
     private HighScoreService highScoreService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<HighScore>> getAllHighScores() {
+        return new ResponseEntity<>(highScoreService.getAllHighScores(), HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/niilo/webbreakout/services/HighScoreService.java
+++ src/main/java/com/niilo/webbreakout/services/HighScoreService.java
@@ -1,12 +1,23 @@
 package com.niilo.webbreakout.services;
 
+import com.niilo.webbreakout.entities.HighScore;
 import com.niilo.webbreakout.repositories.HighScoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class HighScoreService {
 
     private HighScoreRepository highScoreRepository;
+
+    public List<HighScore> getAllHighScores() {
+        return highScoreRepository.findAll();
+    }
+
+    public HighScore saveHighScore(HighScore score) {
+        return highScoreRepository.save(score);
+    }
 }
