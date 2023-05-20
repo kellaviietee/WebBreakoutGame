480ab8ee45083e8a178b29c7a058d9891f488241
kellaviietee <44777425+kellaviietee@users.noreply.github.com>
2023-05-20 20:45:24 +0300

    Started to receive highscore from the game

--- src/main/java/com/niilo/webbreakout/controllers/HighScoreController.java
+++ src/main/java/com/niilo/webbreakout/controllers/HighScoreController.java
@@ -4,9 +4,10 @@ import com.niilo.webbreakout.entities.HighScore;
 import com.niilo.webbreakout.services.HighScoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +18,16 @@ import java.util.List;
 @RequestMapping("/api/highscore")
 public class HighScoreController {
 
-    private HighScoreService highScoreService;
+    private final HighScoreService highScoreService;
 
     @GetMapping("/all")
     public ResponseEntity<List<HighScore>> getAllHighScores() {
         return new ResponseEntity<>(highScoreService.getAllHighScores(), HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<HighScore> addHighScore(@RequestBody HighScore highScore) {
+        return new ResponseEntity<>(highScoreService.saveHighScore(highScore), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/niilo/webbreakout/services/HighScoreService.java
+++ src/main/java/com/niilo/webbreakout/services/HighScoreService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HighScoreService {
 
-    private HighScoreRepository highScoreRepository;
+    private final HighScoreRepository highScoreRepository;
 
     public List<HighScore> getAllHighScores() {
         return highScoreRepository.findAll();
